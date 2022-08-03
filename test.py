9d1411bf13c981085ebc8dc70b96f5e5c8faf4b2
smilerightnow <admane@protonmail.com>
2022-08-03 16:07:02 +0100

    gui

--- test.py
+++ test.py
@@ -9,22 +9,41 @@ g.set_default_cube()
 print(g.get_coordinates())
 
 
-g.y_rotate(30)
-g.x_rotate(30)
+g.y_rotate(0)
+g.x_rotate(0)
 
 print(g.get_coordinates())
 
 g.set_scale(100)
-g.set_pan(250)
+# g.set_pan(250)
 
 
 top = tkinter.Tk()
 
 C = tkinter.Canvas(top, bg="blue", height=500, width=500)
 
+i = 0
 
-for p in g.get_coordinates():
-	C.create_rectangle(p[0], p[1], list(np.array(p) + 5)[0], list(np.array(p) + 5)[1], fill="red")
+def do():
+	C.delete("all")
+	global i
+	i += 0.01
+	g.y_rotate(i)
+	g.x_rotate(i)
+	g.z_rotate(i)
+	for p in g.get_coordinates():
+		C.create_rectangle(p[0], p[1], list(np.array(p) + 5)[0], list(np.array(p) + 5)[1], fill="red")
+	
+	C.after(50, do)
+
 
 C.pack()
+
+
+##making 0 the origin
+C.configure(scrollregion=(-250,-250, 250, 250))
+
+
+do()
+
 top.mainloop()
