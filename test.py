103b5b1a85b39cfbe34ab1b5eea45eed2be14d8f
smilerightnow <admane@protonmail.com>
2022-08-03 19:11:02 +0100

    progress

--- test.py
+++ test.py
@@ -4,16 +4,18 @@ import tkinter
 
 g = Group()
 g.set_default_cube()
+g.set_scale(100)
 
 print(g.get_coordinates())
-
-g.set_scale(100)
 
 top = tkinter.Tk()
 
 C = tkinter.Canvas(top, bg="cyan", height=500, width=500)
+##making 0 the origin
+C.configure(scrollregion=(-250,-250, 250, 250))
 
-def do():
+
+def draw():
 	C.delete("all")
 
 	g.y_rotate((g.x_angle+0.02))
@@ -23,16 +25,9 @@ def do():
 	for p in g.points:
 		C.create_rectangle(p.x, p.y, p.x+5, p.y+5, fill="white")
 	
-	C.after(50, do)
+	C.after(50, draw)
 
 
 C.pack()
-
-
-##making 0 the origin
-C.configure(scrollregion=(-250,-250, 250, 250))
-
-
-do()
-
+draw()
 top.mainloop()
